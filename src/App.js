023e34b15f0e43b287d8d721cac2db8c12a53226
Russell Hart <russellbot@gmail.com>
2020-08-18 21:39:46 -0400

    changed heroku web addresses to localhost

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://still-ocean-85502.herokuapp.com/imageurl', {
+      fetch('http://localhost:3000/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -99,7 +99,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
           if (response) {
-            fetch('https://still-ocean-85502.herokuapp.com/image', {
+            fetch('http://localhost:3000/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
