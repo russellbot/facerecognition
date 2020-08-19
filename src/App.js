e319c79782f4d514eadd52f72710534279cdd7c8
Russell Hart <russellbot@gmail.com>
2020-08-19 14:56:22 -0400

    clean up of calculateFaceLocation function

--- src/App.js
+++ src/App.js
@@ -66,28 +66,18 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFacesArray = data.outputs[0].data.regions;
-    // const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    // console.log(clarifaiFacesArray);
-    const boxArray = clarifaiFacesArray.map(image => {
-      return ({
-        leftCol: image.region_info.bounding_box.left_col * width,
-        topRow: image.region_info.bounding_box.top_row * height,
-        rightCol: width - (image.region_info.bounding_box.right_col * width),
-        bottomRow: height - (image.region_info.bounding_box.bottom_row * height)
-      })
-    })
-    // console.log(boxArray);
-    return (boxArray);
-    // return {
-    //   leftCol: clarifaiFace.left_col * width,
-    //   topRow: clarifaiFace.top_row * height,
-    //   rightCol: width - (clarifaiFace.right_col * width),
-    //   bottomRow: height - (clarifaiFace.bottom_row * height)
-    // }
+    return data.outputs[0].data.regions.map(face => {
+      const clarifaiFace = face.region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    });
   }
 
   displayFaceBox = (boxArray) => {
