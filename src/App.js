7830b827402d24a04ab2e48fb5990cc97acded47
Russell Hart <russellbot@gmail.com>
2020-08-25 11:56:34 -0400

    added user information to Profile

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    pet: '',
+    age: ''
   }    
 }
 
@@ -130,7 +132,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
@@ -140,7 +142,11 @@ class App extends Component {
           toggleModal={this.toggleModal} />
         { isProfileOpen && 
           <Modal>
-            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} />
+            <Profile 
+              isProfileOpen={isProfileOpen} 
+              toggleModal={this.toggleModal}
+              user={user} 
+            />
           </Modal>
         }
         { route === 'home' 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal }) => {
+const Profile = ({ isProfileOpen, toggleModal, user }) => {
     return (
         <div className="profile-modal">
             <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -9,14 +9,14 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <img
                         src="http://tachyons.io/img/logo.jpg"
                         className="br-100 ba h3 w3 dib" alt="avatar" /> 
-                    <h1>John Doe</h1>
-                    <h4>Images Submitted: 5</h4>
-                    <p>Member Since: January</p>
+                    <h1>{user.name}</h1>
+                    <h4>{`Images Submitted: ${user.entries}`}</h4>
+                    <p>{`Member Since: ${new Date(user.joined).toLocaleDateString()}`}</p>
                     <hr />       
                     <label className="mt2 fw6" htmlFor="user-name">Name:</label>
                     <input 
                         className="pa2 ba w-100" 
-                        placeholder="John"
+                        placeholder={user.name}
                         type="text" 
                         name="name"  
                         id="name"  
@@ -24,7 +24,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <label className="mt2 fw6" htmlFor="user-name">Age:</label> 
                     <input 
                         className="pa2 ba w-100" 
-                        placeholder="56"
+                        placeholder={user.age}
                         type="text" 
                         name="user-age"  
                         id="pet"  
@@ -32,7 +32,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
                     <label className="mt2 fw6" htmlFor="user-name">Pet:</label> 
                     <input 
                         className="pa2 ba w-100" 
-                        placeholder="dragon"
+                        placeholder={user.pet}
                         type="text" 
                         name="user-pet"  
                         id="pet"  
