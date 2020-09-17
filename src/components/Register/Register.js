7c061674ac83afb4d5fcec260e173e8441844de3
Russell Hart <russellbot@gmail.com>
2020-09-17 13:15:52 -0400

    added session token to register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -83,7 +83,7 @@ class Register extends React.Component {
                         </fieldset>
                         <div className="">
                             <input 
-                                onClick={this.onSubmitSignIn}
+                                onClick={this.onSubmitRegister}
                                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                                 type="submit" 
                                 value="Register" 
