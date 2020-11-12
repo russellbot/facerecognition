5d84ff585e3593aaa8e59d13be1679792073c916
Russell Hart <russellbot@gmail.com>
2020-11-12 14:30:20 -0500

    updated styles

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -57,15 +57,15 @@ class Signin extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return (
-            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-                <main className="pa4 black-80">
+            <article className="br3 ba white b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input 
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+                                    className="pa2 input-reset ba b--white bg-transparent hover-bg-black hover-white w-100 hover-black" 
                                     type="email" 
                                     name="email-address"  
                                     id="email-address" 
@@ -75,7 +75,7 @@ class Signin extends React.Component {
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input 
-                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 hover-black" 
+                                    className="b pa2 input-reset ba b--white bg-transparent hover-bg-black hover-white w-100 hover-black" 
                                     type="password" 
                                     name="password"  
                                     id="password" 
@@ -86,7 +86,7 @@ class Signin extends React.Component {
                         <div className="">
                             <input 
                                 onClick={this.onSubmitSignIn}
-                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                                className="b ph3 pv2 input-reset ba b--white white bg-transparent grow pointer f6 dib" 
                                 type="submit" 
                                 value="Sign in" 
                             />
