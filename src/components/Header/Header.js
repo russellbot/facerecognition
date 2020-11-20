ad6e5253b225b7aea5a610cef72c916e37f83979
Russell Hart <russellbot@gmail.com>
2020-11-20 17:19:44 -0500

    added title to nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>Smart Face Recognition</h1>    
+            <h1 style={{display: 'flex', justifyContent: 'flex-start'}}>Smart Face Recognition</h1>    
         </div>        
     )
 }
