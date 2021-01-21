1e0a364059d1fa8d7546d8b7e92cf5234a1fd057
Russell Hart <russellbot@gmail.com>
2021-01-21 08:57:37 -0500

    shortened nav bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,14 +5,14 @@ const Navigation = ({ onRouteChange, isSignedIn, toggleModal }) => {
     if(isSignedIn) {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <h1>Smart Face Recognition</h1> 
+                <h1>Smart Face</h1> 
                 <ProfileIcon onRouteChange={onRouteChange} toggleModal={toggleModal} />
             </nav>
         );
     } else {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <h1>Smart Face Recognition</h1> 
+                <h1>Smart Face</h1> 
                 <p onClick={() => onRouteChange('signin')} className='f3 link dim white underline pa3 pointer'>Sign In</p>
                 <p onClick={() => onRouteChange('register')} className='f3 link dim white underline pa3 pointer'>Register</p>
             </nav>
