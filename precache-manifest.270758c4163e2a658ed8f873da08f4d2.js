1b5effc859eb40082fcf5abbb1c0075e033a6045
Russell Hart <russellbot@gmail.com>
2021-02-25 08:56:35 -0500

    Updates

---
+++ precache-manifest.270758c4163e2a658ed8f873da08f4d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50d191ae53623c5c2aad2b7f79e78b9",
+    "url": "/smart-face/index.html"
+  },
+  {
+    "revision": "f8ff35afc3c194aa51df",
+    "url": "/smart-face/static/css/2.cc2d33b2.chunk.css"
+  },
+  {
+    "revision": "f4c70c30b9057e7e8f8f",
+    "url": "/smart-face/static/css/main.6cafabf8.chunk.css"
+  },
+  {
+    "revision": "f8ff35afc3c194aa51df",
+    "url": "/smart-face/static/js/2.15c2a200.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/smart-face/static/js/2.15c2a200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c70c30b9057e7e8f8f",
+    "url": "/smart-face/static/js/main.d6697e24.chunk.js"
+  },
+  {
+    "revision": "766c38b4a421264dd6e8",
+    "url": "/smart-face/static/js/runtime-main.6ece2f61.js"
+  }
+]);
